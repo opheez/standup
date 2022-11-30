f30046e05654884934d2f2a5e93f5c7aa0f8b369
Amy <leiamy12@gmail.com>
2022-11-30 18:20:13 -0500

    Merge feed page into home

---
+++ client/components/Home/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <main class="home">
+    <div v-if="$store.state.email">
+      Reading List
+    </div>
+    <template v-else="$store.state.email">
+      <h2>Welcome to Standup!</h2>
+      <h3>A team project management application for students</h3>
+      <article v-if="showCreate">
+        <RegisterForm/>
+        <p>
+          Already have an account?
+          <a @click="showCreate = false">
+            Sign in
+          </a>
+        </p>
+      </article>
+      <article v-else>
+        <LoginForm/>
+        <p>
+          New to Standup?
+          <a @click="showCreate = true">
+            Create an account
+          </a>
+        </p>
+      </article>
+    </template>
+  </main>
+</template>
+
+<script>
+import RegisterForm from '@/components/Login/RegisterForm.vue';
+import LoginForm from '@/components/Login/LoginForm.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    RegisterForm,
+    LoginForm
+  },
+  data() {
+    return {
+      // whether to show the create acc or login form
+      showCreate: true,
+    };
+  },
+}
+</script>
+
+<style>
+p > a {
+  cursor: pointer;
+  color: indigo;
+}
+</style>
