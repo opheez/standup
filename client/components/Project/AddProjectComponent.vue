900fec4668bdba5c411d219a61c353b659d0812e
Amy <leiamy12@gmail.com>
2022-12-01 16:50:59 -0500

    Add form to add project modal

--- client/components/Project/AddProjectComponent.vue
+++ client/components/Project/AddProjectComponent.vue
@@ -7,21 +7,74 @@
       v-show="show"
       :hideModal="hideModal"
     >
-      <template #title>
-        Add Project
-      </template>
-      <template #body>
-        Form content
-      </template>
-      <template #actions>
-        <button @click="hideModal">Cancel</button>
-        <button>Add</button>
-      </template>
+      <h2>Add Project</h2>
+      <form @submit.prevent="submit">
+        <div class="field">
+          <label for="project-name">
+            Name
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="6.1040 Final Project"
+            :value="fields.name"
+            @input="fields.name = $event.target.value"
+          />
+        </div>
+        <div class="field">
+          <label for="project-name">
+            Deadline
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="date"
+            :min="minDate"
+            :value="fields.deadline"
+            @input="fields.deadline = $event.target.value"
+          />
+        </div>
+        <div class="field">
+          <label for="collaborators">
+            Collaborators
+          </label>
+          <div class="collaborators-list">
+            <div
+              v-for="(collaborator, i) in fields.collaborators"
+              class="collaborator"
+            >
+              <label>
+                ({{i + 1}})
+              </label>
+              <input
+                type="email"
+                :id="i"
+                :name="i"
+                :value="fields.collaborators[i]"
+                placeholder="john.doe@gmail.com"
+                @input="fields.collaborators[i] = $event.target.value"
+              />
+              <button class="text-btn" @click="removeCollaborator(i)">
+                🗑
+              </button>
+            </div>
+            <button class="text-btn" @click="addCollaborator">
+              + Add collaborator
+            </button>
+          </div>
+        </div>
+      </form>
+      <div class="actions">
+        <button class="invert" @click="hideModal">Cancel</button>
+        <button type="submit" @click="submit">Add</button>
+      </div>
     </Modal>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
 import Modal from '@/components/common/Modal.vue';
 
 export default {
@@ -30,6 +83,12 @@ export default {
   data() {
     return {
       show: false,
+      minDate: moment().format('YYYY-MM-DD'),
+      fields: {
+        name: '',
+        deadline: '',
+        collaborators: [''],
+      },
     }
   },
   methods: {
@@ -39,12 +98,54 @@ export default {
     showModal() {
       this.show = true;
     },
-  }
+    submit() {
+      console.log(this.fields);
+      this.hideModal();
+    },
+    addCollaborator() {
+      this.fields.collaborators.push('');
+    },
+    removeCollaborator(i) {
+      this.fields.collaborators.splice(i, 1);
+    }
+  },
 }
 </script>
 
 <style scoped>
-.add-btn {
-  border-color: #50406d;
+.field {
+  margin-bottom: 12px;
+}
+.field > input {
+  width: 100%;
+}
+.collaborators-list {
+  padding: 12px 16px;
+  border: 1px solid #555;
+  background-color: #fff;
+}
+
+.collaborators-list > .text-btn {
+  text-decoration: underline;
+  transition: all 0.2ms ease-in-out;
+}
+.collaborators-list > .text-btn:hover {
+  color: rgb(91, 60, 128);
+}
+.collaborator {
+  display: flex;
+  align-items: center;
+}
+.collaborator > label {
+  margin-right: 8px;
+}
+.collaborator > input {
+  flex-grow: 1;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 24px;
 }
 </style>
