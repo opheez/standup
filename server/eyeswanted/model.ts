ab5ee40428b78ce8f15e560a49facf8e27c828ed
ivy mao <ivymao@mit.edu>
2022-12-02 16:43:18 -0500

    Model for EyesWanted

---
+++ server/eyeswanted/model.ts
@@ -0,0 +1,45 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+import type {User} from '../user/model';
+import type {Update} from '../update/model';
+
+/**
+ * This file defines the properties stored in a EyesWanted
+ * DO NOT implement operations here ---> use collection file
+ */
+
+// Type definition for EyesWanted on the backend
+export type EyesWanted = {
+  _id: Types.ObjectId; // MongoDB assigns each object this ID on creation
+  targetUsers: Types.ObjectId[];
+  updateId: Types.ObjectId;
+};
+
+export type PopulatedEyesWanted = {
+  _id: Types.ObjectId; // MongoDB assigns each object this ID on creation
+  targetusers: User[];
+  updateId: Update;
+};
+
+// Mongoose schema definition for interfacing with a MongoDB table
+// EyesWanted stored in this table will have these fields, with the
+// type given by the type property, inside MongoDB
+const EyesWantedSchema = new Schema<EyesWanted>({
+  // The users who read the EyesWanted
+  targetUsers: [{
+    // Use Types.ObjectId outside of the schema
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }],
+  // The update for the EyesWanted
+  updateId: {
+    // Use Types.ObjectId outside of the schema
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Update'
+  },
+});
+
+const EyesWantedModel = model<EyesWanted>('EyesWanted', EyesWantedSchema);
+export default EyesWantedModel;
