4ff3ced1aa19e5024a711b3af3425715efa56452
Ophelia Zhu <wattinytoffee@gmail.com>
2022-12-03 01:57:09 -0500

    Add project router to index

--- api/index.ts
+++ api/index.ts
@@ -8,6 +8,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import * as userValidator from '../server/user/middleware';
 import {userRouter} from '../server/user/router';
+import { projectRouter } from '../server/project/router';
 // import {freetRouter} from '../server/freet/router';
 import MongoStore from 'connect-mongo';
 
@@ -69,6 +70,7 @@ app.use(userValidator.isCurrentSessionUserExists);
 
 // Add routers from routes folder
 app.use('/api/users', userRouter);
+app.use('/api/projects', projectRouter);
 // app.use('/api/freets', freetRouter);
 
 // Catch all the other routes and display error message
