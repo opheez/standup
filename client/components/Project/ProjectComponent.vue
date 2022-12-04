b5cbb3f457ad32e4ba1dea86cac0ee7f8464ce3c
Amy <leiamy12@gmail.com>
2022-12-04 12:31:23 -0500

    Add ability to archive project

--- client/components/Project/ProjectComponent.vue
+++ client/components/Project/ProjectComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project preview" @click="openProject">
+  <div class="project preview" @click.self="openProject">
     <h3>{{ project.projectName }}</h3>
     <p class="deadline">
       Due: {{ deadline }}
@@ -15,19 +15,49 @@
     >
       {{ status }}
     </div>
+    <div class="edit-container">
+      <dropdown-menu
+        v-if="project.creator === $store.state.email"
+        v-model="showEdit"
+      >
+        <button class="thin-btn invert edit">
+            Edit
+        </button>
+        <div slot="dropdown">
+          <button
+            v-if="project.active"
+            class="dropdown-item"
+            href="#"
+            @click="toggleActive"
+          >Mark inactive</button>
+          <button
+            class="dropdown-item"
+            href="#"
+            @click="markDelete"
+          >Delete</button>
+        </div>
+      </dropdown-menu>
+    </div>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
+import DropdownMenu from '@innologica/vue-dropdown-menu';
 
 export default {
   name: 'ProjectComponent',
+  components: {DropdownMenu},
   props: {
     project: {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    return {
+      showEdit: false,
+    }
   },
   computed: {
     deadline() {
@@ -51,12 +81,40 @@ export default {
           id: this.project._id,
         },
       });
+    },
+    markDelete() {
+      // TODO(AL): call backend when ready
+      this.showEdit = false;
+    },
+    async toggleActive() {
+      const options = {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'same-origin',
+      };
+      try {
+        const res = await fetch(`/api/projects/${this.project._id}`, options);
+        if (!res.ok) {
+          throw Error(resJson.error);
+        }
+        this.$store.commit('alert', {
+          status: 'success',
+          message: 'Successfully marked inactive!',
+        });
+        this.$store.commit('refreshProjects');
+        this.showEdit = false;
+      } catch (e) {
+        this.$store.commit('alert', {
+          status: 'error',
+          message: e,
+        });
+      }
     }
   },
 }
 </script>
 
-<style scoped>
+<style>
 .project {
   display: inline-flex;
   flex-direction: column;
@@ -64,6 +122,7 @@ export default {
   height: 260px;
   margin: 0 12px 12px 0;
   cursor: pointer;
+  position: relative;
 }
 
 .project:hover {
@@ -92,5 +151,55 @@ export default {
   background: #F58870;
 }
 
-
+.project:hover .edit-container {
+  visibility: visible;
+}
+.edit-container {
+  visibility: hidden;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+}
+.dropdown {
+  position: relative;
+} 
+.dropdown-menu.show {
+  display: block;
+}
+.dropdown-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  z-index: 1000;
+  display: none;
+  float: left;
+  min-width: 10rem;
+  padding: 0.5rem 0;
+  margin: 0.125rem 0 0;
+  font-size: 1rem;
+  color: #212529;
+  text-align: left;
+  list-style: none;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #555555;
+  border-radius: 0.25rem;
+} 
+.dropdown-item {
+  display: block;
+  width: 100%;
+  padding: 0.25rem 1.5rem;
+  clear: both;
+  font-weight: 400;
+  color: #212529;
+  text-align: inherit;
+  white-space: nowrap;
+  background-color: transparent;
+  border: 0;
+  text-decoration: none;
+}
+.dropdown-item:hover {
+  background-color: rgba(0,0,0, 0.025);
+}
 </style>
