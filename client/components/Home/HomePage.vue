69c7e64a71df10bd8719daddcac5984d10e8f4c1
Amy <leiamy12@gmail.com>
2022-12-11 16:42:09 -0500

    Move update panel to homepage

--- client/components/Home/HomePage.vue
+++ client/components/Home/HomePage.vue
@@ -1,7 +1,21 @@
 <template>
   <div v-if="$store.state.email">
-    <ReadingList/>
-    <ProjectDashboard/>
+    <main class="left-panel" :class="{thin: $store.state.currentUpdate}">
+      <ReadingList :openUpdate="openUpdate"/>
+      <ProjectDashboard/>
+    </main>
+    <div class="details-container" v-show="$store.state.currentUpdate">
+      <button @click="hideUpdate" class="text-btn thin-btn">
+        Hide >
+      </button>
+      <UpdateDetailPage
+        v-if="$store.state.currentUpdate"
+        class="update-details"
+        :showProjectTitle="true"
+        :project="$store.state.projects.find(p => p._id === $store.state.currentUpdate.projectId)"
+        :update="$store.state.currentUpdate"
+      />
+    </div>
   </div>
   <main class="home" v-else>
     <h2>Welcome to Standup!</h2>
@@ -32,6 +46,7 @@ import RegisterForm from '@/components/Login/RegisterForm.vue';
 import LoginForm from '@/components/Login/LoginForm.vue';
 import ReadingList from '@/components/Home/Readinglist.vue';
 import ProjectDashboard from '@/components/Project/ProjectDashboard.vue';
+import UpdateDetailPage from '@/components/Update/UpdateDetailPage.vue';
 
 export default {
   name: 'HomePage',
@@ -39,7 +54,8 @@ export default {
     RegisterForm,
     LoginForm,
     ReadingList,
-    ProjectDashboard
+    ProjectDashboard,
+    UpdateDetailPage,
   },
   data() {
     return {
@@ -47,12 +63,31 @@ export default {
       showCreate: false,
     };
   },
+  methods: {
+    openUpdate(update) {
+      if (update === this.$store.state.currentUpdate) {
+        this.$store.commit('setCurrentUpdate', null);  
+        return;
+      }
+      this.$store.commit('setCurrentUpdate', update);
+    },
+    hideUpdate() {
+      this.$store.commit('setCurrentUpdate', null);
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
 p > a {
   cursor: pointer;
   color: indigo;
+}
+
+main.left-panel {
+  padding-right: 0;
+}
+main.left-panel.thin {
+  padding-right: 20px;
 }
 </style>
--- client/components/Home/Readinglist.vue
+++ client/components/Home/Readinglist.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="container">
-    <main class="left-panel" :class="{thin: $store.state.currentUpdate}">
-      <h1>
-        Reading List
-      </h1>
-      <template
+  <div>
+    <h1>
+      Reading List
+    </h1>
+    <template
       v-for="eyeswanted in $store.state.eyeswanted">
       <UpdatePreview
         :update="eyeswanted.update"
@@ -13,29 +12,21 @@
     <article v-if="$store.state.eyeswanted.length===0">
       <p> No eyes wanted updates. </p>
     </article>
-    </main>
-    <div class="details-container" v-show="$store.state.currentUpdate">
-      <button @click="hideUpdate" class="text-btn thin-btn">
-        Hide >
-      </button>
-      <UpdateDetailPage
-        v-if="$store.state.currentUpdate"
-        class="update-details"
-        :showProjectTitle="true"
-        :project="$store.state.projects.find(p => p._id === $store.state.currentUpdate.projectId)"
-        :update="$store.state.currentUpdate"
-      />
-    </div>
   </div>
 </template>
   
   <script>
   import UpdatePreview from '@/components/Update/UpdatePreview.vue';
-  import UpdateDetailPage from '@/components/Update/UpdateDetailPage.vue';
   
   export default {
     name: 'ReadingList',
-    components: {UpdatePreview, UpdateDetailPage},
+    components: {UpdatePreview},
+    props: {
+      openUpdate: {
+        type: Function,
+        required: true,
+      }
+    },
     beforeMount() {
       this.$store.commit('refreshEyesWanted');
     },
@@ -43,18 +34,6 @@
       eyeswanted() {
         const eyeswanted = this.$store.state.eyeswanted || [];
         return eyeswanted;
-      }
-    },
-    methods: {
-      openUpdate(update) {
-        if (update === this.$store.state.currentUpdate) {
-          this.$store.commit('setCurrentUpdate', null);  
-          return;
-        }
-        this.$store.commit('setCurrentUpdate', update);
-      },
-      hideUpdate() {
-        this.$store.commit('setCurrentUpdate', null);
       }
     },
   }
